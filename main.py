8d6ccc637ef1e85e0d7b99e4b36930f623845728
IltonBJSilva <xlsonplay@gmail.com>
2023-12-27 11:40:50 -0300

    Novo padrão de estrutura

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import requests
+from flask import Flask, jsonify
 from bd import Carros
 
 app = Flask(__name__)
@@ -6,6 +7,19 @@ app = Flask(__name__)
 
 @app.route('/carros', methods=['GET'])
 def get_carros():
-    return Carros
+    carro_json = jsonify({"carros": Carros})
+    return carro_json
+
+
+
+@app.route("/carros/<int:id>", methods=["GET"])
+def obter_carro(id):
+    carro_encontrado = next((vazio for carro in Carros if carro.get('ID') == id), None)
+    if carro_encontrado:
+        return jsonify(carro_encontrado)
+    return jsonify({"mensagem": "Carro não encontrado"}), 404
+
+
+
 
 app.run()

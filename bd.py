855157b6a299db7a40baa94e4e2e9d17131448cf
IltonBJSilva <xlsonplay@gmail.com>
2023-12-27 09:45:39 -0300

    get carros

---
+++ bd.py
@@ -0,0 +1,122 @@
+Carros = [
+    {
+        "ID": 1, 
+        "Marca": "Toyota", 
+        "Modelo": "Corolla", 
+        "Ano": 2022
+    },
+    {
+        "ID": 2, 
+        "Marca": "Honda", 
+        "Modelo": "Civic", 
+        "Ano": 2021
+    },
+    {
+        "ID": 3, 
+        "Marca": "Ford", 
+        "Modelo": "Focus", 
+        "Ano": 2020
+    },
+    {
+        "ID": 4, 
+        "Marca": "Chevrolet", 
+        "Modelo": "Cruze", 
+        "Ano": 2023
+    },
+    {
+        "ID": 5, 
+        "Marca": "Volkswagen", 
+        "Modelo": "Golf", 
+        "Ano": 2022
+    },
+    {
+        "ID": 6, 
+        "Marca": "Nissan", 
+        "Modelo": "Altima", 
+        "Ano": 2021
+    },
+    {
+        "ID": 7, 
+        "Marca": "Hyundai", 
+        "Modelo": "Elantra", 
+        "Ano": 2020
+    },
+    {
+        "ID": 8, 
+        "Marca": "Mazda", 
+        "Modelo": "Mazda3", 
+        "Ano": 2023
+    },
+    {
+        "ID": 9, 
+        "Marca": "Subaru", 
+        "Modelo": "Impreza", 
+        "Ano": 2022
+    },
+    {
+        "ID": 10, 
+        "Marca": "Kia", 
+        "Modelo": "Forte", 
+        "Ano": 2021
+    },
+    {
+        "ID": 11, 
+        "Marca": "Audi", 
+        "Modelo": "A4", 
+        "Ano": 2020
+    },
+    {
+        "ID": 12, 
+        "Marca": "BMW", 
+        "Modelo": "3 Series", 
+        "Ano": 2023
+    },
+    {
+        "ID": 13, 
+        "Marca": "Mercedes-Benz", 
+        "Modelo": "C-Class", 
+        "Ano": 2022
+    },
+    {
+        "ID": 14, 
+        "Marca": "Lexus", 
+        "Modelo": "IS", 
+        "Ano": 2021
+    },
+    {
+        "ID": 15, 
+        "Marca": "Jaguar", 
+        "Modelo": "XE", 
+        "Ano": 2020
+    },
+    {
+        "ID": 16, 
+        "Marca": "Tesla", 
+        "Modelo": "Model 3", 
+        "Ano": 2023
+    },
+    {
+        "ID": 17, 
+        "Marca": "Porsche", 
+        "Modelo": "Panamera", 
+        "Ano": 2022
+    },
+    {
+        "ID": 18, 
+        "Marca": "Ferrari", 
+        "Modelo": "488 GTB", 
+        "Ano": 2021
+    },
+    {
+        "ID": 19, 
+        "Marca": "Lamborghini", 
+        "Modelo": "Huracan", 
+        "Ano": 2020
+    },
+    {
+        "ID": 20, 
+        "Marca": "Bugatti", 
+        "Modelo": "Chiron", 
+        "Ano": 2023
+    }
+]
